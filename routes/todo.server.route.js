7a19c685c68f6ab75098191934caf6ba6700d19b
john2796 <jbmiranda22796@gmail.com>
2019-01-06 14:28:34 -0800

    creating Schmea for todo server

---
+++ routes/todo.server.route.js
@@ -0,0 +1,19 @@
+const express = require("express");
+// import controller file
+const todoController = require("../controllers/todo.server.controller");
+
+// get an instance of express router
+const router = express.Router();
+
+router
+  .route("/")
+  .get(todoController.getTodos)
+  .post(todoController.addTodo)
+  .put(todoController.updateTodo);
+
+router
+  .route("/:id")
+  .get(todoController.getTodo)
+  .delete(todoController.deleteTodo);
+
+module.exports = router;
