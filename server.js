18dc79a6841d7140cbcf8525a42f0d28e111ebf4
john2796 <jbmiranda22796@gmail.com>
2019-01-06 16:26:13 -0800

    setting up server routes

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const port = 4200;
 
-// middleware
-app.use(bodyParser());
+const db = require("./config/key").MongoURI;
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
+// use bodyparser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/items", itemRouter);
+app.listen(port, () => console.log(`app listening on port ${port}`));
