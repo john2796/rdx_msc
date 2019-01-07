e3cd2979c653b2f6b321b733b52ed045e17bb95c
john2796 <jbmiranda22796@gmail.com>
2019-01-06 16:57:51 -0800

    working on add item comp..

---
+++ client/src/components/AddItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class AddItem extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to add Item</h2>
+      </div>
+    );
+  }
+}
+
+export default AddItem;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddItem from "./components/AddItem";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+  <Router>
+    <div>
+      <Route path="/" component={App} />
+      <Route path="/add-item" component={AddItem} />
+    </div>
+  </Router>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(app, document.getElementById("root"));
 serviceWorker.unregister();
